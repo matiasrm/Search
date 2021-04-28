7dca8392f8753e6749a2e49e943fd6b1d4142af4
Matias Vidoni <matiasrmontenegro@gmail.com>
2021-04-28 16:10:53 -0300

    Initial Commit

---
+++ Search/SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchApp.swift
+//  Search
+//
+//  Created by Mati Montenegro on 28/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
